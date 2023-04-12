da16094184faa96959491fab98260cdb88a01cf7
Singh <richa.j.singh@capgemini.com>
2023-04-12 17:08:20 +0530

    user added

---
+++ OnlineShopping/src/main/java/com/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+private static final long serialVersionUID = 1L;
+	
+	public UserAlreadyExistsException() {
+		
+	}
+	
+	public UserAlreadyExistsException(String message) {
+		
+		super(message);
+	}
+}
---
+++ OnlineShopping/src/main/java/com/exceptions/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public UserNotFoundException() {
+		
+	}
+	
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+}
